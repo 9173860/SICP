a4ca78543405acfc479f7879d78da8706a5286ef
9173860 <yuanjb20@hotmail.com>
2017-01-30 21:29:57 +0800

    Finished 2.40

---
+++ ch2/40.scm
@@ -0,0 +1,9 @@
+(load "accumulate.scm")
+(load "flatmap.scm")
+(load "enumerate-interval")
+
+(define (unique-pairs n)
+  (flatmap (lambda (i) 
+            (map (lambda (j) (list i j)) 
+                 (enumerate-interval 1 (- i 1))))
+            (enumerate-interval 1 n)))
