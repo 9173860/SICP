bdabf8abd94b5bfb4e4f3b29ae0cabd8ec0ba824
9173860 <yuanjb20@hotmail.com>
2017-03-13 23:05:20 +0800

    Finished 2.56

---
+++ ch2/56.scm
@@ -0,0 +1,31 @@
+(load "deriv-methods.scm")
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+(define (base s) (cadr s))
+(define (exponent s) (caddr s))
+(define (make-exponentiation base exponent)
+  (cond ((=number? exponent 0) 1)
+        ((=number? exponent 1) base)
+        (else (list '** base exponent))))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (append exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+           (make-product (exponent exp)
+                         (make-exponentiation (base exp) (- (exponent exp) 1)))
+           (deriv (base exp) var)))
+        (else
+          (error "unknown expression type -- DERIV" exp))))
